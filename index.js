f79139d939699a0bc12c1cf6e40773afeaf23192
石田岳 <gakuishida@ishidatakenoMacBook-ea.local>
2021-09-13 23:38:45 +0900

    クリックする度増加するやつ

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict'
 {
-    
+    const btn = document.getElementById('btn');
+    const count = document.getElementById('count');
+
+    let i = 0;
+
+   btn.addEventListener('click', () => {
+       i++;
+       count.innerHTML = i + '回';
+   });
 }

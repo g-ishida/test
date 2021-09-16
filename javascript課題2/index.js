40268777fb5a4990140451cdeb0d1be7641c86e0
石田岳 <gakuishida@ishidatakenoMacBook-ea.local>
2021-09-16 21:32:15 +0900

    余計なコメントを削除。「＝＝＝」を使用。

--- javascript課題2/index.js
+++ javascript課題2/index.js
@@ -4,18 +4,14 @@
     const fizz = document.getElementById('fizz');
     const buzz = document.getElementById('buzz');
     const button = document.getElementById('button');
-    const number = document.getElementById('number');
-    // ↓動かない？
-    // const fizzNum = fizz.value;
-    // const buzzNum = buzz.value;　　
-
+    const number = document.getElementById('number');　
 
     button.addEventListener('click', () => {
-        if (fizz.value == "" || buzz.value == "") { //空文字なら
+        if (fizz.value === "" || buzz.value === "") { //空文字なら
             window.alert('空文字');
         } else if (!Number.isInteger(fizz.value) || !Number.isInteger(buzz.value)) { //小数なら
             window.alert('小数だよ');
-        } else if (typeof fizz.value == "string" || typeof buzz.value == "string") { //文字列なら
+        } else if (typeof fizz.value === "string" || typeof buzz.value === "string") { //文字列なら
             window.alert('文字列だよ');
         } else {　//エラーがないなら、数字を表示させる
             for (let i = 1; i < 100; i++) {
@@ -35,21 +31,6 @@
             }
         }
 
-        // for (let i = 1; i < 100; i++) {
-        //     let element = document.createElement('p');
-        //     if (i % fizz.value === 0 && i % buzz.value === 0) {
-        //         element.textContent = 'FizzBuzz' + i;
-        //         number.appendChild(element);
-        //     } else if (i % fizz.value === 0) {
-        //         element.textContent = 'Fizz' + i;
-        //         number.appendChild(element);
-        //     } else if (i % buzz.value === 0) {
-        //         element.textContent = 'Buzz' + i;
-        //         number.appendChild(element);
-        //     } else {
-        //         continue;
-        //     }
-        // }
     });
     
     
